58e67b9f705f7b2bba253d8ef8a3b2296ec816a1
Kav. Pather <kav.pather@gmail.com>
2023-06-02 17:22:28 +1000

    feat: add express server.js

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const router = express.Router();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const winston = require("winston");
+
+const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.json(),
+    defaultMeta: { service: "pae-service" },
+    transports: [
+        //
+        // - Write all logs with importance level of `error` or less to `error.log`
+        // - Write all logs with importance level of `info` or less to `combined.log`
+        //
+        new winston.transports.File({ filename: "error.log", level: "error" }),
+        new winston.transports.File({ filename: "combined.log" }),
+    ],
+});
+
+app.use(cors());
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use(express.static(path.join(__dirname, "/src")));
+
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/src/index.html"));
+});
+
+router.get("/game", function (req, res) {
+    res.sendFile(path.join(__dirname, "/src/index.html"));
+});
+
+app.use("/", router);
+app.listen(process.env.port || 3000);
+
+console.log("Listening on port 3000");
